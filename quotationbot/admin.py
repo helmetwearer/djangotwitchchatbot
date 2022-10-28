86f960180522d49f8660d99faf6ffc8b2d3dce93
User <user@Users-MBP.yaboi>
2022-10-28 11:12:26 -0400

    mark to run in list actions so we don't have to hit commnand line

--- quotationbot/admin.py
+++ quotationbot/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+from django_object_actions import DjangoObjectActions
 # Register your models here.
 
 class QuotationAdmin(admin.ModelAdmin):
@@ -10,9 +11,18 @@ class QuotationAdmin(admin.ModelAdmin):
 admin.site.register(Quotation, QuotationAdmin)
 
 
+def set_channels_to_run(modeladmin, request, queryset):
+    for channel in queryset:
+        channel.mark_to_run()
+set_channels_to_run.short_description = 'Mark selected to run now'
+
 class ChannelAdmin(admin.ModelAdmin):
+
     fields = ('name', 'bots_enabled', 'bot_minimum_minutes', 'bot_maximum_minutes', 'enabled_buckets')
     list_display = ('id', 'name', 'bots_enabled')
     search_fields = ('name',)
+
+    actions = [set_channels_to_run, ]
+
     
 admin.site.register(Channel, ChannelAdmin)

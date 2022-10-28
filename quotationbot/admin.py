d9729e59991d623604c77daeb497a124d0773329
User <user@Users-MBP.yaboi>
2022-10-28 11:15:34 -0400

    whoops don't need that old thing

--- quotationbot/admin.py
+++ quotationbot/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import *
-from django_object_actions import DjangoObjectActions
 # Register your models here.
 
 class QuotationAdmin(admin.ModelAdmin):

0947c9154add091c8d74c1772a0b9f11be4f34ab
Daniel Petrikin <danpabsolute@gmail.com>
2022-10-31 15:24:42 -0400

    oh case sensitivity

--- helmetbot/mainviews.py
+++ helmetbot/mainviews.py
@@ -3,4 +3,4 @@ from django.conf import settings
 from django.http import HttpResponse
 
 def readme(request):
-	return HttpResponse(markdown.markdown(open('%s/README.MD' % settings.BASE_DIR, 'r').read()))
+	return HttpResponse(markdown.markdown(open('%s/README.md' % settings.BASE_DIR, 'r').read()))

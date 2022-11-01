886f41de8f0b9dfd2b1fc22c6823ae19a99fc57e
Daniel Petrikin <danpabsolute@gmail.com>
2022-11-01 17:08:59 -0400

    refactoring

--- quotationbot/management/commands/quotationbot.py
+++ quotationbot/management/commands/quotationbot.py
@@ -22,50 +22,72 @@ class Command(BaseCommand, IRCMixin):
             help='server should ignore errors log them and continue running',
         )
 
+
     def verbose_write(self, outputstring):
         if self.verbose_on:
             self.stdout.write(self.style.NOTICE('%s: %s' % (timezone.now(),outputstring)))
 
-    def send_random_quotation_to_channel(self, channel, bucket_name):
+    def send_random_quotation_to_json_channel(self, channel, bucket_name):
         quotations = Quotation.objects.filter(bucket_name=bucket_name, approved=True)
         count = quotations.count()
-        if count:
+        if count and channel.is_bucket_enabled(bucket_name):
             q = quotations[random.randrange(0,count)]
+            results = requests.post(settings.NEEDY_GF_URL, 
+                        data={'phone':channel.name, 'message':q.formatted_text})
 
-            if channel.is_needy_gf_channel:
-                self.verbose_write('Needy gf channel detected')
-
-                results = requests.post(settings.NEEDY_GF_URL, 
-                    data={'phone':channel.name, 'message':q.formatted_text})
-
-                if results.status_code == 200:
-                    self.verbose_write('Needy GF message sent to %s was %s' %(channel.name, q.formatted_text))
-                else:
-                    self.verbose_write('HTTP %s %s' % (results.status_code, results.text))
+            if results.status_code == 200:
+                self.verbose_write('Needy GF message sent to %s was %s' %(channel.name, q.formatted_text))
             else:
-                self.connect()
-                self.verbose_write('IRC connection established')
-                self.send_message(channel.name, q.formatted_text)
-                self.verbose_write('Message sent to %s was %s' %(channel.name, q.formatted_text))   
-                self.close_connection()
-                self.verbose_write('Connection closed')
+                self.verbose_write('HTTP %s %s' % (results.status_code, results.text))
+
+
+    def send_random_quotation_to_twitch_channel(self, channel, bucket_name):
+        quotations = Quotation.objects.filter(bucket_name=bucket_name, approved=True)
+        count = quotations.count()
+        if count and channel.is_bucket_enabled(bucket_name):
+            q = quotations[random.randrange(0,count)]
+            self.connect()
+            self.verbose_write('IRC connection established')
+            self.send_message(channel.name, q.formatted_text)
+            self.verbose_write('Message sent to %s was %s' %(channel.name, q.formatted_text))   
+            self.close_connection()
+            self.verbose_write('Connection closed')
 
 
     def broadcast_available_messages(self, bucket_name):
-        channels = Channel.objects.filter(bots_enabled=True,available_to_message_after__lte=timezone.now())
-        self.verbose_write('Number of channels to write to %s' % channels.count())
-        for channel in channels:
+        available_channels = Channel.objects.filter(
+            bots_enabled=True,available_to_message_after__lte=timezone.now())
+        twitch_channels = available_channels.filter(is_needy_gf_channel=False)
+        imessage_channels = available_channels.filter(is_needy_gf_channel=True)
+        self.verbose_write('Number of twitch channels to write to %s' % twitch_channels.count())
+        for channel in twitch_channels:
             self.verbose_write('checking channel %s' % channel.name)
             if channel.is_live:
                 self.verbose_write('%s came back as live, check if bucket is on' % channel.name)
                 if channel.is_bucket_enabled(bucket_name):
-                    self.send_random_quotation_to_channel(channel, bucket_name)
+                    self.send_random_quotation_to_twitch_channel(channel, bucket_name)
                     channel.available_to_message_after = timezone.now() + timezone.timedelta(
                         minutes=random.randrange(channel.bot_minimum_minutes, channel.bot_maximum_minutes + 1))
                     channel.save()
                     self.verbose_write('%s set to run after %s' % (channel.name, channel.available_to_message_after))
                 else:
                     self.verbose_write('%s bucket not enabled on %s' % (bucket_name, channel.name))
+
+        self.verbose_write('Number of imessage json channels to write to %s' % imessage_channels.count())
+        for channel in imessage_channels:
+            self.verbose_write('checking channel %s for wake time' % channel.name)
+            if channel.is_live:
+                self.verbose_write('%s came back as awake, check if bucket is on' % channel.name)
+                if channel.is_bucket_enabled(bucket_name):
+                    self.send_random_quotation_to_json_channel(channel, bucket_name)
+                    channel.available_to_message_after = timezone.now() + timezone.timedelta(
+                        minutes=random.randrange(channel.bot_minimum_minutes, channel.bot_maximum_minutes + 1))
+                    channel.save()
+                    self.verbose_write('%s set to run after %s' % (channel.name, channel.available_to_message_after))
+                else:
+                    self.verbose_write('%s bucket not enabled on %s' % (bucket_name, channel.name))
+            else:
+                self.verbose_write('channel is sleeping, messages will start upon waking hour')
 
     def handle_sleep(self):
         settings_obj = ChatServerSettings.load()
